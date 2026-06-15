9b5696bef8e382093c2014813a3ec4e00d2e7730
Harry <harrylippa@gmail.com>
2026-06-15 18:17:31 +0100

    Register User with bcrypt

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { pool } from "../db/pool";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
@@ -6,6 +8,22 @@ router.get("/test", (_req, res) => {
   res.json({
     message: "Auth route working",
   });
+});
+
+router.post("/register", async (_req, res) => {
+  const { email, password } = _req.body;
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const result = await pool.query(
+    `
+      INSERT INTO users (email, password_hash)
+      VALUES ($1, $2)
+      RETURNING id, email, created_at
+  `,
+    [email, passwordHash]
+  );
+  res.status(201).json(result.rows[0]);
 });
 
 export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,6 +11,12 @@ const startServer = async () => {
     const result = await pool.query("SELECT NOW()");
     console.log("Database connected:", result.rows[0]);
 
+    const dbName = await pool.query(
+      "SELECT current_database()"
+    );
+
+    console.log("Current database:", dbName.rows[0]);
+
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
