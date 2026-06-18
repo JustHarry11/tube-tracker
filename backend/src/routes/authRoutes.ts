efef189e3f997557b7070d2c68a3d23621c1866b
Harry <harrylippa@gmail.com>
2026-06-18 16:50:25 +0100

    Login Route with error handling

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -4,14 +4,15 @@ import bcrypt from "bcrypt";
 
 const router = Router();
 
-router.get("/test", (_req, res) => {
+router.get("/test", (req, res) => {
   res.json({
     message: "Auth route working",
   });
 });
 
-router.post("/register", async (_req, res) => {
-  const { email, password } = _req.body;
+// Register a new user
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body;
 
   const passwordHash = await bcrypt.hash(password, 10);
 
@@ -24,6 +25,52 @@ router.post("/register", async (_req, res) => {
     [email, passwordHash]
   );
   res.status(201).json(result.rows[0]);
+});
+
+// Login a user
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const result = await pool.query(
+      `
+      SELECT * FROM users WHERE email = $1
+  `,
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const user = result.rows[0];
+    const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user.id,
+        email: user.email,
+      },
+    });
+    
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
 });
 
 export default router;
